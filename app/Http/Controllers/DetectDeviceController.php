3b4d2a9a0c7b1f46c30ae8dddc25678ef111fa5f
Mohammed Fahjan <fahjan@gmaiol.com>
2026-02-10 22:36:47 +0200

    redirect to store

--- app/Http/Controllers/DetectDeviceController.php
+++ app/Http/Controllers/DetectDeviceController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Jenssegers\Agent\Facades\Agent;
 
 class DetectDeviceController extends Controller
 {
     public function __invoke()
     {
+        if (Agent::isPhone()) {
+            if (Agent::is('iOS')) {
+                return redirect()->to('https://testflight.apple.com/join/Cf4eUbrY');
+            }
+            if (Agent::is('Android')) {
+                return redirect()->to('https://play.google.com/store/apps/details?id=app.test.ps');
+                return redirect()->to('https://play.google.com/apps/testing/app.test.ps');
+            }
+        }
+
         return ['app' => 'web'];
     }
 }
