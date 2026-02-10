8d1e5bc6aa4e8a730bc61892a78ff5d493718b69
Mohammed Fahjan <fahjan@gmaiol.com>
2026-02-10 22:01:52 +0200

    create user

--- app/Http/Requests/CreateStudentRequest.php
+++ app/Http/Requests/CreateStudentRequest.php
@@ -40,6 +40,9 @@ class CreateStudentRequest extends FormRequest
             'trainer_id' => 'required',
             'drivingtrainer_id' => 'required',
             'gender' => 'required',
+            'agreed_lessons' => ['required', 'integer'],
+
+
         ];
     }
 
