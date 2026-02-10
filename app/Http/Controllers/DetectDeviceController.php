a0495a95330b6deed776671e82104f117ab11ac1
Mohammed Fahjan <fahjan@gmaiol.com>
2026-02-10 22:46:37 +0200

    redirect to store

--- app/Http/Controllers/DetectDeviceController.php
+++ app/Http/Controllers/DetectDeviceController.php
@@ -3,20 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Jenssegers\Agent\Facades\Agent;
+// use Jenssegers\Agent\Facades\Agent;
+use Jenssegers\Agent\Agent;
 
 class DetectDeviceController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        if (Agent::isPhone()) {
-            if (Agent::is('iOS')) {
-                return redirect()->to('https://testflight.apple.com/join/Cf4eUbrY');
-            }
-            if (Agent::is('Android')) {
-                return redirect()->to('https://play.google.com/store/apps/details?id=app.test.ps');
-                return redirect()->to('https://play.google.com/apps/testing/app.test.ps');
-            }
+
+        $agent = new Agent();
+
+        // if (Agent::isPhone()) {
+        //     if (Agent::is('iOS')) {
+        //         return redirect()->to('https://testflight.apple.com/join/Cf4eUbrY');
+        //     }
+        //     if (Agent::is('Android')) {
+        //         return redirect()->to('https://play.google.com/store/apps/details?id=app.test.ps');
+        //         return redirect()->to('https://play.google.com/apps/testing/app.test.ps');
+        //     }
+        // }
+
+        if ($agent->isAndroidOS()) {
+            return redirect()->to('https://play.google.com/store/apps/details?id=app.test.ps');
+        }
+
+        if ($agent->isiPhone()) {
+            return redirect()->to('https://testflight.apple.com/join/Cf4eUbrY');
         }
 
         return ['app' => 'web'];
