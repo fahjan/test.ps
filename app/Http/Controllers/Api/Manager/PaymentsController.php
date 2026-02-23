d2582630b63fa97380f7f8dbf6eb33fcf4e8ad86
Mohammed Fahjan <fahjan@gmaiol.com>
2026-02-23 08:27:57 +0200

    return payment with student object latest created at

--- app/Http/Controllers/Api/Manager/PaymentsController.php
+++ app/Http/Controllers/Api/Manager/PaymentsController.php
@@ -22,12 +22,12 @@ class PaymentsController extends Controller
 
         $page = request()->page ?? 1;
         return cache()->rememberForever("student-payments:$student->id-page:$page", function () use ($student) {
-            return $student->payments()->with('kind')->latest('created_at')->paginate(10);
+            // return $student->payments()->with('kind')->latest('created_at')->paginate(10);
 
-            // $payments = Payment::
-            //     with(['kind', 'student'])
-            //     ->where("student_id", $student->id)->latest('id')->simplePaginate(10);
-            // return PaymentResource::collection($payments);
+            $payments = Payment::
+                with(['kind', 'student'])
+                ->where("student_id", $student->id)->latest('created_at')->simplePaginate();
+            return PaymentResource::collection($payments);
         });
     }
 
