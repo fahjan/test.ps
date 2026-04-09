d6b44d2d249e46f32a0bfac7e481e8201cea5c89
Mohammed Fahjan <fahjan@gmaiol.com>
2026-04-09 13:14:32 +0300

    except trainer for school to add student

--- app/Http/Requests/ListTrainersRequest.php
+++ app/Http/Requests/ListTrainersRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\Manager;
+use App\Models\Trainer;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ListTrainersRequest extends FormRequest
@@ -12,13 +13,25 @@ class ListTrainersRequest extends FormRequest
      */
     public function authorize(): bool
     {
+        // list trainers for manager and trainers 
+
+        // check if the current user is manager of the school, if not return false
         $manager_has_role_to_school = Manager::whereHas('user', function ($q) {
             $q->where('id', auth()->id());
         })->whereHas('school', function ($q) {
             return $q->where('id', request('school_id'));
         })->exists();
 
-        return $manager_has_role_to_school;
+
+        // check if the current user is trainer of the school, if not return false
+
+        $trainer_has_role_to_school = Trainer::whereHas('user', function ($q) {
+            $q->where('id', auth()->id());
+        })->whereHas('school', function ($q) {
+            return $q->where('id', request('school_id'));
+        })->exists();
+
+        return $manager_has_role_to_school || $trainer_has_role_to_school;
     }
 
     /**
