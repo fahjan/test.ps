568e6b32bdc1e3ded49125ccf5e38088dde5a53e
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-09 18:08:32 +0300

    admin stats route

--- app/Http/Controllers/Api/Admin/StatsController.php
+++ app/Http/Controllers/Api/Admin/StatsController.php
@@ -11,16 +11,22 @@ class StatsController extends Controller
     public function __invoke(Request $request)
     {
         $students = \App\Models\Student::count();
-        $students_this_month = \App\Models\Student::whereMonth('created_at', now()->month)
-            ->whereYear('created_at', now()->year)
+
+        $students_this_week = \App\Models\Student::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])
             ->count();
+
+        $students_this_month = \App\Models\Student::whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->count();
+
+        // $students_this_month = \App\Models\Student::whereMonth('created_at', now()->month)
+        //     ->whereYear('created_at', now()->year)
+        //     ->count();
 
         $students_by_day = \App\Models\Student::selectRaw('DATE(created_at) as date, COUNT(*) as count')
             ->groupBy('date')
             ->orderBy('date', 'desc')
             ->get();
-        $students_this_week = \App\Models\Student::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])
-            ->count();
+
 
         return response()->json([
             'students_count' => $students,
