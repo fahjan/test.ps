2120e72f9e740bb037c3fed640674a42c84d1525
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-09 17:41:53 +0300

    get admin students stats

---
+++ app/Http/Controllers/Api/Admin/StatsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StatsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $students = \App\Models\Student::count();
+        $students_by_day = \App\Models\Student::selectRaw('DATE(created_at) as date, COUNT(*) as count')
+            ->groupBy('date')
+            ->orderBy('date', 'desc')
+            ->get();
+
+        return response()->json([
+            'students' => $students,
+            'students_by_day' => $students_by_day,
+        ]);
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
