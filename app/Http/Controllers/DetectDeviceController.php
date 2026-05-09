ab9040bac8496659b7ffb3f6170e8392a0530a00
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-09 13:32:53 +0300

    change android app to haiia server

--- app/Http/Controllers/DetectDeviceController.php
+++ app/Http/Controllers/DetectDeviceController.php
@@ -12,7 +12,8 @@ class DetectDeviceController extends Controller
 
         $app_store = 'https://apps.apple.com/us/app/test-ps-driving-school-gaza/id6758583111';
         // $google_play = 'https://play.google.com/store/apps/details?id=app.test.ps';
-        $google_play = 'https://test.ps/app/app.apk';
+        // $google_play = 'https://test.ps/app/app.apk';
+        $google_play = 'http://t.haiia.com/apk/app.apk';
 
         $agent = new Agent();
 
