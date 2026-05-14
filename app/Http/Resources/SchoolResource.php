a010feaa15fef36f27a82f141388d82b067cd50e
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-14 10:42:34 +0300

    return school object in payout

--- app/Http/Resources/SchoolResource.php
+++ app/Http/Resources/SchoolResource.php
@@ -17,7 +17,7 @@ class SchoolResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'logo' => $this->logo == null ? null : asset($this->logo),
+            'logo' => $this->logo == null ? '' : asset($this->logo),
         ];
     }
 }
