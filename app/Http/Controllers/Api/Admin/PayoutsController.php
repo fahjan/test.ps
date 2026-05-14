996c232846dec8a13aa6686d45d5d996b92140e7
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-14 10:05:50 +0300

    add school shate' logo

--- app/Http/Controllers/Api/Admin/PayoutsController.php
+++ app/Http/Controllers/Api/Admin/PayoutsController.php
@@ -42,7 +42,7 @@ class PayoutsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Payout $payout)
     {
         //
     }
