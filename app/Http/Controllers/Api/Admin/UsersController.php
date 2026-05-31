07fd06faef79db541acea354aa0d6f7f94308a0a
Mohammed Fahjan <fahjan@gmaiol.com>
2026-05-31 10:13:21 +0300

    send sms to user after update password

--- app/Http/Controllers/Api/Admin/UsersController.php
+++ app/Http/Controllers/Api/Admin/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SimpleUserResource;
 use App\Models\User;
+use App\Notifications\SendPassword;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
@@ -20,6 +21,10 @@ class UsersController extends Controller
         User::where('id_number', $request->id_number)->update(['password' => Hash::make($code), 'code' => $code]);
 
         $user = User::where('id_number', $request->id_number)->first();
+
+        $sms_provider = [];
+
+        $user->notify(new SendPassword(['message' => 'اسم المستخدم: ' . ltrim($user['mobile'], '97') . ' : ' . 'كلمة المرور: ' . $code . ', ' . url('/app')], $sms_provider));
 
         return new SimpleUserResource($user);
     }
